45e9394613da4aad201ef5bbba0f5e48d78ec02a
Боташева Л.Б <115580268+LarisaBotasheva@users.noreply.github.com>
2023-01-07 14:15:09 +0300

    Исправленный task_1
    Здравствуйте! Я правильно поняла какие методы нужно было добавлять?
    Подскажите, пожалуйста, какой курс я могу рассмотреть по Python, чтобы лучше в нем разбираться. У меня ощущение, что я упустила базовые моменты и поэтому теперь не всегда понимаю что происходит....

--- Лабораторная 1/task_1.py
+++ Лабораторная 1/task_1.py
@@ -1,4 +1,6 @@
 # TODO Написать 3 класса с документацией и аннотацией типов
+import doctest
+
 class MultiCooker:
     def __init__(self, settings_amount: int, volume: float, height: int):
         if not isinstance(settings_amount, int):
@@ -10,6 +12,15 @@ class MultiCooker:
         if not isinstance(height, int):
             raise TypeError("Высота мультиварки должна быть типа int")
         self.volume = volume
+    def is_empty_MultiCooker(self) -> bool:
+        ...
+    def add_food_to_MultiCooker(self, food: float) -> None:
+        if not isinstance(food, (int, float)):
+            raise TypeError("Добавляемая в мультиварку еда должна быть типа int или float")
+        if food < 0:
+            raise ValueError("Добавляемая еда должна быть положительным числом")
+    def remove_food_from_MultiCooker(self, amount_of_food: float) -> None:
+        ...
 
 class Toy:
     def __init__(self, colour: str, mass: float):
@@ -19,7 +30,16 @@ class Toy:
         if mass < 0:
             raise ValueError("Игрушка не может весить меньше 0 кг")
         self.mass = mass
-
+    def cut_toys_fur(self, cm: float) -> None:
+        if not isinstance(cm, (int, float)):
+            raise TypeError("Отрезать мех можно только в формате int или float")
+        if cm <= 0:
+            raise ValueError("Нельзя отрезать 0 или меньше сантиметров меха")
+    def play_with_the_toy(self, time: float) -> None:
+        ...
+    def throw_away_the_toy(self, amount: int) -> None:
+        ...
+        
 class Towel:
     def __init__(self, colour_towel: str, length: int, width: int):
         if not isinstance(colour_towel, str):
@@ -31,7 +51,16 @@ class Towel:
         if not isinstance(width, int):
             raise TypeError("Ширину полотенца лучше указать через int")
         self.width =  width
-
+    def wash_towel(self, amount_of_times: int) -> None:
+        if not isinstance(amount_of_times, int):
+            raise TypeError("Количество раз, которые вы стираете полотенца должно быть целочисленным")
+        if amount_of_times < 0:
+            raise ValueError("Если < 0, то вы не стирали полотенце")
+    def throw_away_the_towel(self, times: int) -> None:
+        ...
+    def fold_the_towel(self, amount_of_folds: int) -> None:
+        ...
+        
 if __name__ == "__main__":
     Redmond = MultiCooker(7, 4.5, 25)
     Bosch = MultiCooker(10, 5.2, 28)
@@ -39,7 +68,7 @@ if __name__ == "__main__":
     Owl = Toy("коричневый", 0.2)
     Face = Towel('серый', 40, 20)
     Body = Towel('синий', 120, 60)
-    import doctest
+    
     doctest.testmod()
     # TODO работоспособность экземпляров класса проверить с помощью doctest
     pass
