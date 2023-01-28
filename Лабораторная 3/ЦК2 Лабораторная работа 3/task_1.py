009e6fee70012cb6c579b49af98e2d9792fbebc9
Боташева Л.Б <115580268+LarisaBotasheva@users.noreply.github.com>
2023-01-29 02:43:43 +0300

    Лабораторная работа 3

---
+++ Лабораторная 3/ЦК2 Лабораторная работа 3/task_1.py
@@ -0,0 +1,48 @@
+class Book:
+    """ Базовый класс книги. """
+
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        if isinstance(pages, int):
+            if pages > 0:
+                self.pages = pages
+            else:
+                raise ValueError("Число страниц в книге должно быть > 0")
+        else:
+            raise TypeError("Число страниц должно быть типа int")
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Количество страниц {self.pages}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        if isinstance(duration, float):
+            if duration > 0:
+                self.duration = duration
+            else:
+                raise ValueError("Длительность книги должна быть > 0")
+        else:
+            raise TypeError("Длительность книги должна быть типа float")
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}. Длительность {self.duration}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, duration={self.duration!r})"
